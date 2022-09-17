b37cd47586d4070499bf596374707153dbd9856c
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2022-09-17 16:07:04 -0300

    Adiciona cano

--- core/src/br/com/inottec/inottecBird.java
+++ core/src/br/com/inottec/inottecBird.java
@@ -11,6 +11,8 @@ public class inottecBird extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture[] passaros;
 	private Texture fundo;
+	private Texture canoBaixo;
+	private Texture canotopo;
 
 	// Atributos de configuracao
 	private int larguraDispositivo;
@@ -19,6 +21,9 @@ public class inottecBird extends ApplicationAdapter {
 	private float variacao = 0;
 	private float velocidadeQueda = 0;
 	private float posicaInicialVertical;
+	private float posicaoMovimentoCanoHorizontal;
+	private float espacoEntreCanos;
+	private float deltaTime;
 
 	@Override
 	public void create () {
@@ -30,16 +35,21 @@ public class inottecBird extends ApplicationAdapter {
 		passaros[2] = new Texture("passaro3.png");
 
 		fundo = new Texture("fundo.png");
+		canoBaixo = new Texture("cano_baixo_maior.png");
+		canotopo = new Texture("cano_topo_maior.png");
 
 		larguraDispositivo = Gdx.graphics.getWidth();
 		alturaDispositivo = Gdx.graphics.getHeight();
 		posicaInicialVertical = alturaDispositivo / 2;
+		posicaoMovimentoCanoHorizontal = larguraDispositivo-100;
+		espacoEntreCanos = 300;
 
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		posicaoMovimentoCanoHorizontal -=  Gdx.graphics.getDeltaTime()*5;
 		velocidadeQueda++;
 
 		variacao += Gdx.graphics.getDeltaTime() * 5;
@@ -56,7 +66,8 @@ public class inottecBird extends ApplicationAdapter {
 		batch.begin();
 
 		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
-
+        batch.draw(canotopo, posicaoMovimentoCanoHorizontal,alturaDispositivo/2 + espacoEntreCanos/2);
+        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal,alturaDispositivo/2 - canoBaixo.getHeight() - espacoEntreCanos/2);
 		batch.draw(passaros [(int)variacao], 30, posicaInicialVertical);
 		batch.end();
 	}
