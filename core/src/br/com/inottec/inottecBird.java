a1257703dea4ffc349b2d4a03f7cfa270da1d798
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2022-09-17 18:00:11 -0300

    movimentação do cano

--- core/src/br/com/inottec/inottecBird.java
+++ core/src/br/com/inottec/inottecBird.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.Random;
+
 public class inottecBird extends ApplicationAdapter {
 
 	private SpriteBatch batch;
@@ -13,6 +15,7 @@ public class inottecBird extends ApplicationAdapter {
 	private Texture fundo;
 	private Texture canoBaixo;
 	private Texture canotopo;
+	private Random numeroRandomico;
 
 	// Atributos de configuracao
 	private int larguraDispositivo;
@@ -24,11 +27,13 @@ public class inottecBird extends ApplicationAdapter {
 	private float posicaoMovimentoCanoHorizontal;
 	private float espacoEntreCanos;
 	private float deltaTime;
+	private float alturaEntreCanosRandomica;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 
+		numeroRandomico = new Random();
 		passaros = new Texture[3];
 		passaros[0] = new Texture("passaro1.png");
 		passaros[1] = new Texture("passaro2.png");
@@ -41,7 +46,7 @@ public class inottecBird extends ApplicationAdapter {
 		larguraDispositivo = Gdx.graphics.getWidth();
 		alturaDispositivo = Gdx.graphics.getHeight();
 		posicaInicialVertical = alturaDispositivo / 2;
-		posicaoMovimentoCanoHorizontal = larguraDispositivo-100;
+		posicaoMovimentoCanoHorizontal = larguraDispositivo;
 		espacoEntreCanos = 300;
 
 	}
@@ -49,10 +54,13 @@ public class inottecBird extends ApplicationAdapter {
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		posicaoMovimentoCanoHorizontal -=  Gdx.graphics.getDeltaTime()*5;
+
+		deltaTime = Gdx.graphics.getDeltaTime();
+
+		posicaoMovimentoCanoHorizontal -=  deltaTime * 200;
 		velocidadeQueda++;
 
-		variacao += Gdx.graphics.getDeltaTime() * 5;
+		variacao += deltaTime * 5;
 		if(variacao > 2) variacao = 0;
 
 		if(Gdx.input.justTouched()){
@@ -63,12 +71,18 @@ public class inottecBird extends ApplicationAdapter {
 			posicaInicialVertical = posicaInicialVertical - velocidadeQueda;
 		}
 
+		//verifica se o cano saiu inteiramente da tela
+		if (posicaoMovimentoCanoHorizontal < - 100){
+			posicaoMovimentoCanoHorizontal = larguraDispositivo;
+			alturaEntreCanosRandomica = numeroRandomico.nextInt(1000)-250;
+		}
+
 		batch.begin();
 
 		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
-        batch.draw(canotopo, posicaoMovimentoCanoHorizontal,alturaDispositivo/2 + espacoEntreCanos/2);
-        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal,alturaDispositivo/2 - canoBaixo.getHeight() - espacoEntreCanos/2);
-		batch.draw(passaros [(int)variacao], 30, posicaInicialVertical);
+        batch.draw(canotopo, posicaoMovimentoCanoHorizontal,alturaDispositivo/2 + espacoEntreCanos / 2 + alturaEntreCanosRandomica);
+        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal,alturaDispositivo/2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaEntreCanosRandomica);
+		batch.draw(passaros [(int)variacao], 140, posicaInicialVertical);
 		batch.end();
 	}
 	
